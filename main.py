3fd0463623509db09c7d27d7698e25b0b046204a
ttpr0 <ttpro@gmx.de>
2025-04-27 22:05:23 +0200

    add code

---
+++ main.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2023 Authors of the MCDA project - All Rights Reserved
+
+from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import logging
+
+# create application
+app = FastAPI()
+
+# configure cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
+async def hello():
+    logging.info("/hello endpoint called")
+    return {"message": "Hello World"}
+
+if __name__ == '__main__':
+    # start application
+    uvicorn.run("main:app", host="0.0.0.0", port=5000)
